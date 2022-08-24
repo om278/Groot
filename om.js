ea72a66db0c5423a61d2e155637ce82b61141e28
Om Shende <110690841+om278@users.noreply.github.com>
2022-08-24 20:00:36 +0530

    Update om.js

--- om.js
+++ om.js
@@ -32,14 +32,50 @@ recognition.addEventListener("result", (e) => {
       p.innerText = "My Name is Badshaha";
       texts.appendChild(p);
     }
+    if (
+      text.includes("who is your boss") ||
+      text.includes("who is your boss")
+    ) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "OM SHENDE";
+       console.log("opening Instagram");
+      window.open("https://www.Instagram.com/omn_shende?utm_source=qr");
+      texts.appendChild(p);
+    }
     if (text.includes("open YouTube")) {
       p = document.createElement("p");
       p.classList.add("replay");
-      p.innerText = "opening youtube";
+      p.innerText = "opening youtube boss";
       texts.appendChild(p);
       console.log("opening youtube");
       window.open("https://www.youtube.com");
     }
+    if (text.includes("open Instagram")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "opening Instagram Boss";
+      texts.appendChild(p);
+      console.log("opening Instagram");
+      window.open("https://www.Instagram.com");
+    }
+    if (text.includes("open email")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "opening email";
+      texts.appendChild(p);
+      console.log("opening youtube");
+      window.open("https://mail.google.com");
+    }
+if (text.includes("open Whatsapp")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "opening Whatsapp";
+      texts.appendChild(p);
+      console.log("opening Whatsapp");
+      window.open("https://wa.me.com");
+    }
+    
     p = document.createElement("p");
   }
 });
