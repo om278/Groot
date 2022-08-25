8b06813345f5dd9642b9e26f3df846322cf619d1
Om Shende <110690841+om278@users.noreply.github.com>
2022-08-25 19:38:26 +0530

    Update om.js

--- om.js
+++ om.js
@@ -32,6 +32,39 @@ recognition.addEventListener("result", (e) => {
       p.innerText = "My Name is Badshaha";
       texts.appendChild(p);
     }
+     if (
+      text.includes("play a song") ||
+      text.includes("play a song")
+    ) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "how is your mood sir...";
+      texts.appendChild(p);
+    }
+      if (text.includes("sad")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "playing Arjit singh's song";
+      texts.appendChild(p);
+      console.log("playing Arjit singh's song");
+      window.open("https://youtu.be/_51KXfwcPMs");
+    }
+     if (text.includes("happy")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "playing party song";
+      texts.appendChild(p);
+      console.log("playing party song");
+      window.open("https://youtu.be/bzW9fmwcmG4");
+    }
+     if (text.includes("romantic")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "playing romantic song";
+      texts.appendChild(p);
+      console.log("playing romantic song");
+      window.open("https://youtu.be/NK5cxNJz1Uo");
+    }
     if (
       text.includes("who is your boss") ||
       text.includes("who is your boss")
@@ -59,6 +92,14 @@ recognition.addEventListener("result", (e) => {
       console.log("opening Instagram");
       window.open("https://www.Instagram.com");
     }
+     if (text.includes("open facebook")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "opening facebook Boss";
+      texts.appendChild(p);
+      console.log("opening facebook");
+      window.open("https://www.facebook.com");
+    }
     if (text.includes("open email")) {
       p = document.createElement("p");
       p.classList.add("replay");
@@ -75,7 +116,23 @@ if (text.includes("open WhatsApp")) {
       console.log("opening WhatsApp");
       window.open("https://wa.me");
     }
+    if (text.includes("whats's the time")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "showing a time.....";
+      texts.appendChild(p);
+      console.log("showing a time.....");
+      window.open("https://time");
+    }
     
+    if (text.includes("make a call")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "calling.....";
+      texts.appendChild(p);
+      console.log("calling.....");
+      window.open("const simulateCall = 9359086635 => window. open(`tel:${9359086635}`, '_self');");
+    }
     p = document.createElement("p");
   }
 });
