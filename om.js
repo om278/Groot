0876a998a8d8563bef635616c93375b1837b5db6
Om Shende <110690841+om278@users.noreply.github.com>
2022-10-22 20:01:26 +0530

    Update om.js

--- om.js
+++ om.js
@@ -146,7 +146,7 @@ if (text.includes("open WhatsApp")) {
 
 
 
-	 if (text.includes('what is') || text.includes('who is') || text.includes('what are') || text.includes(' ')) {
+	 if (text.includes('what is') || text.includes('who is') || text.includes('what are') || text.includes('search')) {
       p = document.createElement("p");
       p.classList.add("replay");
       p.innerText = "showing.....";
